30a834674d7e4f6fe2f0cd913c6f8866805c5315
Eric McDaniel <McDanielES@gmail.com>
2019-06-19 00:15:38 -0500

    Circled game loop, begin error checking

--- QWERTYKing.java
+++ QWERTYKing.java
@@ -3,27 +3,42 @@ import java.util.*;
 public class QWERTYKing
 {
     public static final int GAME_SIZE = 10;
+
     public static void main(String[] args)
     {
         try
         {
-            FileReader wordsFile = new FileReader("3000Words.txt");
-            ArrayList<String> words = loadFile(wordsFile);
-            String[] inGameWords = new String[GAME_SIZE];
+            boolean           continueGame = true;
+            FileReader        wordsFile    = new FileReader("3000Words.txt");
+            ArrayList<String> words        = loadFile(wordsFile);
+            ArrayList<String> inGameWords  = new ArrayList<String>();
 
-            System.out.printf("Duplicate the following words.\n------------------------------\n");
-            for (int i = 0; i < GAME_SIZE; ++i)
+            while (continueGame)
             {
-                inGameWords[i] = getRandomWord(words);
-                System.out.printf("%s ", inGameWords[i]);
-            }
-            System.out.printf("\n\n");
+                System.out.printf("Duplicate the following words.\n------------------------------\n");
+                for (int i = 0; i < GAME_SIZE; ++i)
+                {
+                    inGameWords.add(getRandomWord(words));
+                    System.out.printf("%s ", inGameWords.get(i));
+                }
+                System.out.printf("\n\n");
 
-            ArrayList<String> userTypedWords = fillUserInputArray(new Scanner(System.in).nextLine());
+                ArrayList<String> userTypedWords = fillUserInputArray(new Scanner(System.in).nextLine());
 
-            if (!validInput(userTypedWords))
-            {
-                System.out.printf("You did not type the same number of words as provided. There should be %d words.\n", GAME_SIZE);
+                if (validInput(userTypedWords))
+                {
+                    ArrayList<Integer> errors = getUserErrors(inGameWords, userTypedWords);
+                    if (errors.size() > 0)
+                        printErrors(inGameWords, userTypedWords, errors);
+
+                }
+                else
+                    System.out.printf("You did not type the same number of words as provided. "
+                                   + "There should be %d words.\n\n", GAME_SIZE);
+
+                System.out.printf("Continue? Enter y/n: ");
+                continueGame = !(new Scanner(System.in).next().toLowerCase().charAt(0) == 'n') ? true : false;
+                System.out.println();
             }
 
             
@@ -66,5 +81,22 @@ public class QWERTYKing
         while (scan.hasNext())
             userInput.add(scan.next().trim());
         return userInput;
+    }
+
+    public static ArrayList<Integer> getUserErrors(ArrayList<String> inGameWords,
+                                                   ArrayList<String> userTypedWords)
+    {
+        ArrayList<Integer> userErrors = new ArrayList<Integer>();
+        for (int i = 0; i < inGameWords.size(); ++i)
+            if (!(inGameWords.get(i).trim().equalsIgnoreCase(userTypedWords.get(i).trim())))
+                userErrors.add(i);
+        return userErrors;
+    }
+
+    public static void printErrors(ArrayList<String> inGameWords,
+                                   ArrayList<String> userTypedWords,
+                                   ArrayList<Integer> errors)
+    {
+
     }
 }
