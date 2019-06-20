9341dcc12db42c550c133399fee6f30139a77395
Eric McDaniel <McDanielES@gmail.com>
2019-06-20 00:59:13 -0500

    Adjust logic flow, handle negative integer args

--- QWERTYKing.java
+++ QWERTYKing.java
@@ -1,5 +1,4 @@
 import java.io.FileReader;
-import java.io.FileNotFoundException;
 import java.util.*;
 public class QWERTYKing
 {
@@ -11,34 +10,39 @@ public class QWERTYKing
     {
         try
         {
-            java.util.List<String> argsList = java.util.Arrays.asList(args);
-            String  customFile = (argsList.size() > 1 && argsList.contains("-f")) ? (argsList.get(argsList.indexOf("-f") + 1)) : DEFAULT_FILE;
-            int     gameSize   = (argsList.size() > 1 && argsList.contains("-s")) ? (Integer.parseInt(argsList.get(argsList.indexOf("-s") + 1))) : GAME_SIZE;
-            boolean randomCaps = (argsList.size() > 0 && argsList.contains("-C")) ? !DEFAULT_CAPS : DEFAULT_CAPS;
             System.out.printf("Type quit to end the game at any time.\nDuplicate the following words.\n\n");
 
-            boolean    continueGame = true;
-            FileReader wordsFile    = new FileReader(customFile);
-            Words      gameWords    = new Words(wordsFile, gameSize, randomCaps);
+            List<String> argsList     =  Arrays.asList(args);
+            String       customFile   = (argsList.size() > 1 && argsList.contains("-f")) ? (argsList.get(argsList.indexOf("-f") + 1)) : DEFAULT_FILE;
+            int          customSize   = (argsList.size() > 1 && argsList.contains("-s")) ? (Integer.parseInt(argsList.get(argsList.indexOf("-s") + 1))) : GAME_SIZE;
+            boolean      randomCaps   = (argsList.contains("-C")) ? !DEFAULT_CAPS : DEFAULT_CAPS;
+            boolean      continueGame = true;
+            FileReader   wordsFile    = new FileReader(customFile);
+            Words        gameWords    = new Words(wordsFile,
+                                                 (customSize > 0) ? customSize : GAME_SIZE, // Discard negative integers to default size
+                                                  randomCaps);
 
             // Game loop logic
             while (continueGame)
-            {
+            {   // The statement resolves a weird bug where the object prints things
+                // in improper order. I'm not sure why, however this solution works.
+                try { Thread.sleep(5); } catch (InterruptedException ex) {}
+
                 gameWords.fillAndPrintRandomWords();
                 gameWords.getUserEntry();
 
-                if (gameWords.validInput())
+                if (gameWords.validInput() && !gameWords.quitGame())
                     gameWords.printAnyErrors();
                 else if (gameWords.quitGame())
                     continueGame = false;
                 else
                     System.err.printf("You did not type the same number of words as provided. "
-                                   + "There should be %d words.\n\n", gameSize);
+                                    + "There should be %d words.\n\n", customSize);
 
                 gameWords.clear();
-            } // End game loop            
+            } // End game loop
         }
-        catch (FileNotFoundException ex)
+        catch (java.io.FileNotFoundException ex)
         {
             System.err.println("Error locating file. Terminating.");
             System.exit(1);
